3a2d948eef8d1d7c2c507f6419210f878db68294
Tahalokhandwala790 <imgamerrr@gmail.com>
2025-03-15 14:52:00 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Tiza Webcrafts",
+  description: "Modern & Minimal Tiza webcarfts Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="logo.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        <script
+          src="//code.tidio.co/tnhgyxjh9scckhtc3409bqpfh3w1e7am.js"
+          async
+        ></script>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,223 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Balancing creativity and precision in design.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexible and adaptable to client time zones.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Continuously refining my design skills.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Delivering high-quality design solutions.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "",
+  },
+
+  {
+    id: 5,
+    title: "Expanding my design expertise.",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Ready to Work !!! ",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Fizzi",
+    des: "Our soda is made with real fruit juice and a touch of cane sugar. We never use artificial sweeteners or high fructose corn syrup. Try all five flavors and find your favorite!",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg"],
+    link: "https://fizzi-tz.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "ClayOut Catlog",
+    des: "Explore Clayout catalog, carefully selected to elevate your designs. From textures to patterns, discover the perfect elements to bring your vision to life",
+    img: "/p2.svg",
+    iconLists: [],
+    link: "https://tizawebcrafts-portfolio.my.canva.site/clayout-shitabi-faovur-catalog",
+  },
+  {
+    id: 3,
+    title: "Brand Banner - 1",
+    des: "Elevate your brand presence with our stunning banner design, crafted to capture attention and leave a lasting impression",
+    img: "/KID FAVOUR.svg",
+    iconLists: [],
+    link: "https://tizawebcrafts-portfolio.my.canva.site/kid-favour",
+  },
+  {
+    id: 4,
+    title: "Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://i-phone-clone-tz.netlify.app",
+  },
+  {
+    id: 5,
+    title: "Brand Banner - 2",
+    des: "Elevate your brand presence with our stunning banner design, crafted to capture attention and leave a lasting impression",
+    img: "/Ramadan.svg",
+    iconLists: [],
+    link: "https://ramadancollection.my.canva.site/",
+  },
+
+]
+
+export const testimonials = [
+  {
+    quote:
+      "The team at Tiza Webcrafts did an exceptional job building our company's new website. They were highly responsive, understood our requirements perfectly, and delivered a visually stunning and user-friendly platform. Highly recommended for anyone looking for top-notch web development services.",
+    name: "Neha Sharma",
+    title: "Director of AlphaStream Technologies",
+    img: "review1.webp",
+  },
+  {
+    quote:
+      "Working with Tiza Webcrafts was a breeze. They guided us through the entire web development process, offering valuable insights and suggestions to optimize our online presence. The final product exceeded our expectations, and we've seen a significant increase in website traffic and leads since launch.",
+    name: "Raj Kumar",
+    title: "Director of AlphaStream Technologies",
+    img: "review2.webp",
+  },
+  {
+    quote:
+      "As a small business owner, I was looking for a reliable web development partner to help establish my online store. Tiza Webcrafts not only built a visually appealing and functional e-commerce website, but they also provided ongoing support and optimization recommendations. I'm thrilled with the results and highly recommend their services.",
+    name: "Priya Gupta",
+    title: "Director of AlphaStream Technologies",
+    img: "review3.avif",
+  },
+  {
+    quote:
+      "Tiza Webcrafts demonstrated exceptional technical expertise and project management skills during the development of our corporate website. They were proactive in identifying and addressing any issues, ensuring a seamless launch. I'm impressed by their dedication and would gladly work with them again.",
+    name: "Anish Mehta",
+    title: "Director of AlphaStream Technologies",
+    img: "review4.avif",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Web Designer",
+    desc: "Designed intuitive and responsive websites, prioritizing user-centered design principles.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "UI/UX Designer",
+    desc: "Improved user interfaces and experiences, focusing on usability, accessibility, and aesthetics.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Graphic Designer ( Still Learning )",
+    desc: "Crafted engaging graphics, logos, and icons, leveraging design principles to convey messages effectively.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  ];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/tahalokhandwala790",
+    target: "_blank"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "",
+    target: ""
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "",
+    target: ""
+  },
+];
